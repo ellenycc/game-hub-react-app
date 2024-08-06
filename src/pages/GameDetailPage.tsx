ff65f78760553f459debe4d70fe1d78ceaa74880
Ellen CHan <ellenycc@gmail.com>
2024-08-06 15:29:52 +0100

    Set up routing

---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+const GameDetailPage = () => {
+  return (
+    <>
+      <NavBar />
+      <div>GameDetailPage</div>
+    </>
+  );
+};
+
+export default GameDetailPage;
